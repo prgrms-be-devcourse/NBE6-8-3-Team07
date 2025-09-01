f64e13d2879447e424132920dd2e215e09a71c37
johnbosco0414 <johnbosco0414@gmail.com>
2025-09-01 16:21:50 +0900

    [refactor]backendapplicationtests kotlin 전환

---
+++ backend/src/test/java/com/back/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.back
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+        // Spring Boot 애플리케이션 컨텍스트가 정상적으로 로드되는지 확인하는 테스트
+    }
+}
