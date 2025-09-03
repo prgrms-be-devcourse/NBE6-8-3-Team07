15a825664dde8a22108a82a260cdc14441a8313d
yongho9064 <dydgmldydgh@naver.com>
2025-09-03 10:29:24 +0900

    feat: 레디스 의존성 추가 및 설정

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -32,6 +32,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.redisson:redisson-spring-boot-starter:3.29.0")
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("com.h2database:h2")
